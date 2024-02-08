f230a3766fd11dbd57e3b521654aeb6d8e1a5a9e
enmanuelmag <enmanuelmag@cardor.dev>
2024-02-08 15:38:43 -0500

    feat: add dummy test

---
+++ packages/web/src/app/App.test.tsx
@@ -0,0 +1,9 @@
+import { describe, test } from 'vitest'
+//import { render, screen } from '@testing-library/react';
+//import App from '@pages/content/ui/app';
+
+describe('appTest', () => {
+  test('render text', () => {
+    // given
+  })
+})
