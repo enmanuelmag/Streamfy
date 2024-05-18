905511888b6a6e63476c599a632d411a8864dfab
enmanuelmag <enmanuelmag@cardor.dev>
2024-05-18 08:08:45 -0500

    fix: deploy

--- packages/functions/src/services/discord.ts
+++ packages/functions/src/services/discord.ts
@@ -3,15 +3,16 @@ import { Client, Events, GatewayIntentBits, Guild } from 'discord.js'
 import { Logger } from '@global/utils'
 
 class DiscordClient {
-  private client: Client
+  private client?: Client
   private instance: DiscordClient | null = null
   private guildClient: Guild | null = null
 
   private BOT_TOKEN = process.env.VITE_DISCORD_BOT_TOKEN
 
   constructor() {
-    if (!this.BOT_TOKEN) {
-      throw new Error('No bot token provided')
+    if (!this.BOT_TOKEN && process.env.GITHUB_USER) {
+      Logger.error('Skipping Discord login, missing BOT_TOKEN on GitHub Actions')
+      return
     }
 
     this.client = new Client({ intents: [GatewayIntentBits.Guilds] })
@@ -24,6 +25,10 @@ class DiscordClient {
   async getInstance(guildId: string) {
     if (!this.instance) {
       this.instance = new DiscordClient()
+    }
+
+    if (!this.instance.client) {
+      throw new Error('Discord client not initialized')
     }
 
     const guilds = this.instance.client.guilds.cache
